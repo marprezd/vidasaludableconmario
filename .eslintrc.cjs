de9ce1f569ad38459c4e75ff895aa09a9e2d3ee9
marprezd <marprezd@gmail.com>
2026-06-10 17:14:08 -0500

    chore(ESLint): :art: Added an .eslintrc.cjs file that exports @antfu/eslint-config.

---
+++ .eslintrc.cjs
@@ -0,0 +1,3 @@
+// Use the shared Antfu ESLint config to satisfy the ESLint VS Code extension
+// and avoid the "Configure" code action popup on save.
+module.exports = require('@antfu/eslint-config')
